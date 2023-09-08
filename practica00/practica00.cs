8d64f2700bf4d56b0160f45adfed5a2902c89062
TecNM <TecNM@DESKTOP-BABKH5A>
2023-09-08 11:55:34 -0600

    Agregar archivos de proyecto.

---
+++ practica00/practica00.cs
@@ -0,0 +1,10 @@
+﻿
+double b, h, a;
+Console.WriteLine("Ingresa base");
+b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingresa altura");
+h = Convert.ToDouble(Console.ReadLine());
+a = b * h / 2;
+Console.WriteLine("El área del triangulo es");
+Console.WriteLine(a);
+Console.ReadLine();
